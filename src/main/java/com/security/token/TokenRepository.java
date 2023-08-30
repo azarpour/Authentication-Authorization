fdd018b9ce6179d4e2c9e8c00067288a05c4e123
free <mahshidazarpour@gmail.com>
2023-08-29 20:52:44 -0500

    First commit

---
+++ src/main/java/com/security/token/TokenRepository.java
@@ -0,0 +1,20 @@
+package com.security.token;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+
+  @Query(value = """
+      select t from Token t inner join _user u\s
+      on t.authUserDetails.id = u.id\s
+      where u.id = :id and (t.expired = false or t.revoked = false)\s
+      """)
+  List<Token> findAllValidTokenByUser(Long id);
+
+  Optional<Token> findByToken(String token);
+}
